0dd5d725580f0258399e407369f5c5c21eff8e0a
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2024-05-11 23:58:16 -0400

    fix faulty env.sh conda path

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@
 env_name="${1:-base}"
 
 source ~/.bashrc
-source ~/mambaforge/etc/profile.d/mamba.sh
+source $(conda info --base)/etc/profile.d/mamba.sh
 eval "$(conda shell.bash hook)"
 conda activate "$env_name"
 shift
